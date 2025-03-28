4dbae8f613db7a7e9497a2fb734f01f63b6f9651
YusufXojimurodov <xojimurodovyusufjon2@gmail.com>
2025-03-28 20:40:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let ism = prompt('Ismingizni kiriting')
+let familya = prompt('Familyangizni kiriting')
+let sharif = prompt('Sharifingiz ?')
+let yoshingiz = prompt('Yoshingiz ?')
+let telefon = prompt('Telefon Raqamingiz ?')
+let manzil = prompt('Yashash manzilingiz ?')
+let ota = prompt('Otangizni Ismi ?')
+let ona = prompt('Onangizni ismi ?')
+let oqish = prompt('Maktabingiz raqami ?')
+let kurslar = prompt('Qanday kurslarga borasiz  ?')
+
+let javob = alert(
+    ' Ismingiz : ' + ism + 
+    ' Familyangiz : ' + familya + 
+    ' Sharifingiz : ' + sharif + 
+    ' Yoshingiz : ' + yoshingiz +
+    ' Telefon raqamingiz : ' + telefon +
+    ' Manzilingiz : ' + manzil +
+    ' Otangiz ismi : ' + ota +
+    ' Onangiz ismi : ' + ona +
+    ' Maktabingiz raqami : ' + oqish +
+    ' Boradigan kurslaringiz : ' + kurslar
+)
